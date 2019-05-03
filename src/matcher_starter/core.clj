6703e4a5b80a13f2b18a39ba7cb6e534c75c79d8
Stuart Gleeson <T7097354@live.tees.ac.uk>
2019-05-03 03:38:12 +0100

    Initial commit

---
+++ src/matcher_starter/core.clj
@@ -0,0 +1,36 @@
+(ns matcher-starter.core
+  (:require [org.clojars.cognesence.breadth-search.core :refer :all]
+            [org.clojars.cognesence.matcher.core :refer :all]
+            [org.clojars.cognesence.ops-search.core :refer :all]))
+
+(use 'clojure.java.io)
+
+(defn readFirstLine
+  [rdr]
+  (with-open [rdr (reader "Input.txt")]
+    (def firstLine (nth (line-seq rdr) (dec 1)))
+    (let [fLine firstLine] fLine)))
+
+(defn readFile
+  []
+  (with-open [rdr (reader "Input.txt")]
+    (def amount (readFirstLine rdr))
+    (println (str amount "\n---"))
+    (line-seq rdr)
+      (doseq [line (line-seq rdr)]
+        (def charString(clojure.string/split line #""))
+        (def charLineVector(map-indexed vector charString))
+        ;(println charStringVector)
+        (doseq [char charLineVector]
+          (print char)
+        )
+          ;(print (if (= char "1") char nil))
+        (println "")
+      )
+  )
+)
+
+(defn writeLine
+  []
+  (with-open [wrtr (writer "Output.txt" :append true)]
+    (.write wrtr "\ntest2")))
